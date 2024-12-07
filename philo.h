c43c1ec3c4bc56370d4109735e880d9fb551ec72
Armand Mititelu <armitite@fu-r8-p8.s19.be>
2024-12-07 23:09:04 +0100

    Bonne gestion mutex jcrois

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: armitite <armitite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 08:59:01 by armitite          #+#    #+#             */
-/*   Updated: 2024/12/06 15:57:07 by armitite         ###   ########.fr       */
+/*   Updated: 2024/12/07 21:04:13 by armitite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,25 +37,33 @@
 
 // } t_data;
 
-// typedef struct s_philo
-// {
-//     int n;
-//     int fork_left;
-//     int fork_right;
-//     t_data *data;
-//     pthread_mutex_t mutex_print;
-
-// } t_philo;
-
 typedef struct s_test
 {
     int n;
+    int i;
+    int ph_nbr;
+    int starving_time;
+    int eating_time;
+    int sleeping_time;
+    int meals_nbr;
+    int death;
     // int fork_left;
     // int fork_right;
     // t_data *data;
     pthread_mutex_t *mutex_forks;
 
 } t_test;
+
+typedef struct s_philo
+{
+    int n;
+    int fork_left;
+    int fork_right;
+    t_test *data;
+    pthread_mutex_t mutex_print;
+
+} t_philo;
+
 
 int	ft_usleep(long int time);
 int	ft_atoi(const char *str);
--- test2.c
+++ test2.c
@@ -2,28 +2,59 @@
 
 void    *routine(void *arg)
 {
-    t_test *test = (t_test *)arg;
-    pthread_mutex_t *left_fork = &test->mutex_forks[test->n];
-    pthread_mutex_t *right_fork = &test->mutex_forks[(test->n + 1) % 3];
+    t_philo *p = (t_philo *)arg;
+    if (p->n % 2 == 0)
+        ft_usleep(150);
+    if (p->n == 0)
+        p->data->n = 13;
+    pthread_mutex_t *left_fork = &p->data->mutex_forks[p->n];
+    pthread_mutex_t *right_fork = &p->data->mutex_forks[(p->n + 1) % 3];
     pthread_mutex_lock(right_fork);
     pthread_mutex_lock(left_fork);
-    printf("%d is taking fork\n", test->n);
-    printf("%d is eating\n", test->n);
-    usleep(2000);
+    printf("%d is taking fork\n", p->n);
+    printf("%d is eating\n", p->n);
+    ft_usleep(p->data->eating_time);
     pthread_mutex_unlock(right_fork);
     pthread_mutex_unlock(left_fork);
-    printf("%d is sleeping\n", test->n);
-    //ft_usleep(2000);
+    printf("%d is sleeping\n", p->n);
+    printf("%d\n", p->data->n);
+    ft_usleep(p->data->sleeping_time);
+}
+
+int stock_args(t_test *data, int ac, char **av)
+{
+    data->ph_nbr= ft_atoi(av[1]);
+    data->starving_time = ft_atoi(av[2]);
+    data->eating_time = ft_atoi(av[3]);
+    data->sleeping_time = ft_atoi(av[4]);
+    if (ac == 6)
+        data->meals_nbr = ft_atoi(av[5]);
+    else
+        data->meals_nbr = -1;
 }
 
 int main(int ac, char **av)
 {
-    pthread_t th[3];
     int i = 0;
     t_test  test;
-    test.mutex_forks = malloc(sizeof(pthread_mutex_t) * 3);
-    while (i < 3)
+    t_philo **philo;
+    if (ac == 6 || ac == 5)
+        stock_args(&test, ac, av);
+    pthread_t th[test.ph_nbr];
+    philo = malloc(sizeof(t_philo *) * test.ph_nbr);
+    while (i < test.ph_nbr)
     {
+        philo[i] = malloc(sizeof(t_philo) * test.ph_nbr);
+        philo[i]->data = malloc (sizeof(t_test));
+        philo[i]->data = &test;
+        philo[i]->n = i;
+        i++;
+    }
+    i = 0;
+    test.mutex_forks = malloc(sizeof(pthread_mutex_t) * test.ph_nbr);
+    while (i < test.ph_nbr)
+    {
+        test.i = i;
         if (pthread_mutex_init(&test.mutex_forks[i], (void *)&test) != 0)
         {
             perror("Mutex initialization failed");
@@ -37,16 +68,16 @@ int main(int ac, char **av)
         }
         i++;
     }
-    for (i = 0; i < 3; i++) 
+    for (i = 0; i < test.ph_nbr; i++) 
     {
         test.n = i;
-        if (pthread_create(th + i, NULL, &routine, (void *)&test) != 0) {
+        if (pthread_create(th + i, NULL, &routine, (void *)philo[i]) != 0) {
             perror("Failed to create thread");
             return 1;
         }
         printf("Thread %d has started\n", i);
     }
-    for (i = 0; i < 3; i++) 
+    for (i = 0; i < test.ph_nbr; i++) 
     {
         if (pthread_join(th[i], NULL) != 0) {
             return 2;
